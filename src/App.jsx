f36f3082e89d899773a0e57cf5c5a142e6ea7737
lizwachira <lizwanguwachira@gmail.com>
2024-05-02 15:42:50 +0300

    assignment done

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./layout/Header"
+import Footer from "./layout/Footer"
+
+function App() {
+
+
+  return (
+  
+    <div>
+      <Header />
+      
+     Home 
+     <Footer />
+    </div>
+    
+  )
+}
+
+export default App
---
+++ src/layout/Header.jsx
@@ -0,0 +1,14 @@
+
+const Header = () => {
+  return (
+    <div>
+      Header
+      <h1>React Starter</h1>
+    </div>
+   
+  )
+}
+
+export default Header
+
+
